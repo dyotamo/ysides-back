1b940634b63305e1ecfedd04e2664a2aa4551675
Dássone J <dyotamo@gmail.com>
2020-06-10 12:21:24 +0200

    Adding tests.

---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+from models import Entity, Question
+
+
+class TestModels(unittest.TestCase):
+    def test_entity_map(self):
+        entity = Entity(
+            name='Dássone', email='dyotamo@gmail.com', password='xyz')
+        self.assertEqual(entity.to_map(), {
+                         'name': 'Dássone', 'email': 'dyotamo@gmail.com'})
+
+
+if __name__ == '__main__':
+    unittest.main()
