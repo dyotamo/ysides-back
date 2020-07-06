b8c24957d07d5ba2bac05c1a6b20be8d58761178
Dássone J <dyotamo@gmail.com>
2020-07-06 12:39:37 +0200

    Hiding host.

--- application.py
+++ application.py
@@ -139,4 +139,4 @@ api.add_resource(QuestionDetailResource, '/api/questions/<int:question_id>')
 api.add_resource(VoteResource, '/api/vote')
 
 if __name__ == '__main__':
-    application.run(debug=True, host='0.0.0.0')
+    application.run(debug=True)
